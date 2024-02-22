1bd5888ed41b5b211df77983613ec375a456db4b
david.leon <david.leon@epn.edu.ec>
2024-02-21 23:44:57 -0500

    new commit

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,16 +1,20 @@
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+// Generar una clave secreta segura
+const secretKey = crypto.randomBytes(64).toString('hex');
 
 export const createAccessToken = (payload) => {
   return new Promise((resolve, reject) => {
     jwt.sign(
       payload,
-      "xyz123", // Se debe utilizar una clave secreta segura para firmar el token
+      secretKey, // Utiliza la clave secreta generada
       {
-        expiresIn: "1d", // El token expirará después de 1 día
+        expiresIn: "1d",
       },
       (err, token) => {
-        if (err) reject(err); // Si hay un error, se rechaza la promesa
-        resolve(token); // Si no hay errores, se resuelve la promesa con el token generado
+        if (err) reject(err);
+        resolve(token);
       }
     );
   });
